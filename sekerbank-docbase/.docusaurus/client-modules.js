94bae867616a5fb3de5657b0e3bba86ae9d63e64
Bogac Kucuk <bogackucuk@outlook.com>
2023-10-22 19:39:45 +0300

    project added

---
+++ sekerbank-docbase/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\bogac\\Desktop\\dev\\seker\\sekerbank-docbase\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\bogac\\Desktop\\dev\\seker\\sekerbank-docbase\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\bogac\\Desktop\\dev\\seker\\sekerbank-docbase\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\bogac\\Desktop\\dev\\seker\\sekerbank-docbase\\src\\css\\custom.css'),
+];
